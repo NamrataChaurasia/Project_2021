a9036af9e19d41e2beef93880cc926a801fe0c72
Namrata Chaurasia <76465327+NamrataChaurasia@users.noreply.github.com>
2021-02-05 19:24:23 +0530

    Delete MessageQueue/CRUDwithRESTandJPA directory

--- MessageQueue/CRUDwithRESTandJPA/src/main/java/com/capgemini/dao/EmployeeDAO.java
+++
@@ -1,9 +0,0 @@
-package com.capgemini.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.capgemini.beans.Employee;
-
-public interface EmployeeDAO extends JpaRepository<Employee, String> {
-
-}
